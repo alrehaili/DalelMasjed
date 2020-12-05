521b85ea228865b69b315d8221bbd450d20d3bb7
RahimDesk <mth_41@hotmail.com>
2020-12-05 18:43:48 +0300

    Mans Comment

--- app/src/main/java/com/example/dalelmasjed/helper/WebServices.java
+++ app/src/main/java/com/example/dalelmasjed/helper/WebServices.java
@@ -7,3 +7,10 @@ public class WebServices {
     public static String URL_Addcomment="https://madinahhost2020.com/DalelMasjed/API/AddComment.php";
     public static String URL_Getcomment = "https://madinahhost2020.com/DalelMasjed/API/getcomment.php";
 }
+
+
+/*
+
+Manso Mans
+
+ */
